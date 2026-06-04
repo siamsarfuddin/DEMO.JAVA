793b7284522d5f65b0dc1eba70c4c7056ba1a204
siamsarfuddin <siamsarfuddin201821@gmai.com>
2026-06-04 02:21:07 -0700

    this is learing phase

--- New/Near.java
+++ New/Near.java
@@ -42,5 +42,6 @@ public class Near {
         System.out.println("siam sarfuddin seheikh");
         System.out.println("siam sarfuddin seheikh");
         System.out.println("siam sarfuddin seheikh");
+         System.out.println("siam sarfuddin seheikh");
     }
 }
