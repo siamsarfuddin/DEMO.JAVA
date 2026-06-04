93f10f10e807e083af580f520d44d8cf17141a99
siamsarfuddin <siamsarfuddin201821@gmai.com>
2026-06-04 02:56:17 -0700

    this is a wellcome message

--- New/Near.java
+++ New/Near.java
@@ -44,5 +44,6 @@ public class Near {
         System.out.println("siam sarfuddin seheikh");
          System.out.println("siam sarfuddin seheikh");
         System.out.println("siam sarfuddin seheikh");
+        System.out.println("siam sarfuddin seheikh");
     }
 }
