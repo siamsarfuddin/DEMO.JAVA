59ac372da850ba40c35360ca54c67c969b952dbb
siamsarfuddin <siamsarfuddin201821@gmai.com>
2026-06-04 02:17:51 -0700

    this is learing phase of git

--- New/Near.java
+++ New/Near.java
@@ -40,6 +40,7 @@ public class Near {
          System.out.println("siam sarfuddin seheikh");
         System.out.println("siam sarfuddin seheikh");
         System.out.println("siam sarfuddin seheikh");
+        System.out.println("siam sarfuddin seheikh");
     
     }
 }
