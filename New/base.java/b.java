daca431906d38e585f098c2bc5419f5ea3540b0e
siamsarfuddin <siamsarfuddin201821@gmai.com>
2026-06-08 08:06:09 -0700

    this is java coding

--- New/base.java/b.java
+++ New/base.java/b.java
@@ -1,5 +1,26 @@
-package base.java;
+
+import java.util.*;
 
 public class b {
-    
+
+    public static void main(String[] args) {
+        int number = 13;
+
+
+        for(int i=0;i<13;i++){
+
+ 
+        if ((i% 2 == 0&& i%3==0)||(i%5==0&& i%6==0)) {
+            
+            System.out.println("this is an even numberS " +i);
+        }
+        
+        else {
+            System.out.println("this is an odd number " +i);
+
+        }
+
+    }
+
+}
 }
